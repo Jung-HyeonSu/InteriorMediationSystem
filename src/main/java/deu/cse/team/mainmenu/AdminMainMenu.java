2abf6d0486048327f392df3df33078dbdbb25da0
Hyeonsu <gustn05233@naver.com>
2022-06-08 19:35:56 +0900

    견적서 삭제 기능 추가

--- src/main/java/deu/cse/team/mainmenu/AdminMainMenu.java
+++ src/main/java/deu/cse/team/mainmenu/AdminMainMenu.java
@@ -4,6 +4,7 @@
  */
 package deu.cse.team.mainmenu;
 import deu.cse.team.adminfunc.AdminfuncGui;
+import deu.cse.team.adminfunc.EstimateMgmt;
 import deu.cse.team.login.Login;
 import deu.cse.team.observer.Notice;
 
@@ -37,6 +38,7 @@ public class AdminMainMenu extends javax.swing.JFrame {
         SignOutButton = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -69,6 +71,13 @@ public class AdminMainMenu extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setText("견적서 관리");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -93,7 +102,8 @@ public class AdminMainMenu extends javax.swing.JFrame {
                         .addGap(143, 143, 143)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap(136, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -105,11 +115,13 @@ public class AdminMainMenu extends javax.swing.JFrame {
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
+                .addGap(38, 38, 38)
                 .addComponent(jButton2)
-                .addGap(43, 43, 43)
+                .addGap(28, 28, 28)
+                .addComponent(jButton3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(30, 30, 30)
+                .addGap(16, 16, 16)
                 .addComponent(SignOutButton)
                 .addGap(17, 17, 17))
         );
@@ -137,6 +149,12 @@ public class AdminMainMenu extends javax.swing.JFrame {
                 
                 
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        EstimateMgmt estimateMgmt = new  EstimateMgmt();
+        estimateMgmt.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -177,6 +195,7 @@ public class AdminMainMenu extends javax.swing.JFrame {
     private javax.swing.JButton SignOutButton;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     public javax.swing.JLabel jLabel3;
